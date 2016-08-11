8311b56a9355377ded80b58eb3e56ae1288e29f4
jeff <jf0423@gmail.com>
2016-08-11 23:13:06 +0800

    initial commit

--- app/index.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import {TodoApp} from '../app/TodoApp.js';
-
-
-
-
-ReactDOM.render(<TodoApp />, document.getElementById('app'));
